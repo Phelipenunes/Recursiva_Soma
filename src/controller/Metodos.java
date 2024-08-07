2498c28221cac0aeb93eeef351c836b6d4e6febd
Phelipe <phelipesenac@gmail.com>
2024-08-07 15:43:32 -0300

    .

--- src/controller/Metodos.java
+++ src/controller/Metodos.java
@@ -8,7 +8,7 @@ public class Metodos {
 		if(valor <= 0){
 			return 0;// se o valor for menor que 0 retornar 0.
 		}else{
-			return valor + soma(valor - 1);// soma o valor atual com o valor anteria a ele ate chegar ao 0
+			return valor + soma(valor - 1);// soma o valor atual com o valor anterior a ele ate chegar ao 0
 		}
 	}
 }
